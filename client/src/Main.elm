0f9c96cee70718a1a4e4ee4bb07dc01d0a5ac832
gigobyte <gigobest2@gmail.com>
2018-10-15 23:29:28 +0300

    SPA setup

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,4 +1,60 @@
-import Html
+module Main exposing (main)
+
+import Browser exposing (Document, document)
+import Browser.Navigation as Nav
+import Html exposing (..)
+import Url
+
+
+type alias Model =
+    { key : Nav.Key
+    , url : Url.Url
+    }
+
+
+type Msg
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd msg )
+init flags url key =
+    ( { key = key, url = url }, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | url = url }, Cmd.none )
+
+
+view : Model -> Document msg
+view model =
+    { title = "Hello world"
+    , body = [ text "Hello world" ]
+    }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
 
 main =
-  Html.text "Hello World"
+    Browser.application
+        { init = init
+        , subscriptions = subscriptions
+        , update = update
+        , view = view
+        , onUrlRequest = LinkClicked
+        , onUrlChange = UrlChanged
+        }
