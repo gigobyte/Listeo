c119a4c88a66d499fb9c9ce97d526d3d23d2df48
gigobyte <gigobest2@gmail.com>
2018-10-31 21:50:12 +0200

    Refactoring

--- server/src/Feature/User/HTTP.hs
+++ server/src/Feature/User/HTTP.hs
@@ -10,7 +10,6 @@ import           Feature.User.Service      as Service
 import           Feature.User.Types        (RegisterBody (..),
                                             RegisterError (..),
                                             RegisterResponse (..))
-import qualified Feature.User.UserModel    as User
 import           Infrastructure.DB         as DB
 import           Infrastructure.Maybe      (maybeToEither)
 import qualified Network.HTTP.Types.Status as Status
@@ -18,15 +17,8 @@ import           Protolude                 hiding (ByteString, maybeToEither)
 import           Web.Scotty                (ActionM, body, json, status)
 
 toHttpResult :: Either RegisterError a -> ActionM ()
-toHttpResult (Left err) = json $ RegisterResponse { errorDescription = err }
+toHttpResult (Left err) = json $ RegisterResponse err
 toHttpResult _          = status Status.ok200
-
-mkUser :: Time.UTCTime -> RegisterBody -> Either RegisterError User.User
-mkUser dateNow req =
-    maybeToEither ValidationFailed $ User.User
-        <$> (User.mkUsername $ username req)
-        <*> (User.mkPassword $ password req)
-        <*> pure dateNow
 
 parseBody :: ByteString -> Either RegisterError RegisterBody
 parseBody b =
@@ -40,7 +32,7 @@ register pipe = do
     result <- liftIO
             . DB.runQuery pipe
             . (Service.insertUser <$>)
-            . (mkUser dateNow =<<)
+            . (Service.mkUser dateNow =<<)
             . parseBody
             $ rawBody
 
--- server/src/Feature/User/Service.hs
+++ server/src/Feature/User/Service.hs
@@ -1,17 +1,25 @@
 module Feature.User.Service where
 
+import qualified Data.Time.Clock        as Time
 import           Database.MongoDB       (Action, Value)
 import           Feature.User.DB        as DB
-import           Feature.User.Types     (RegisterError (..))
+import           Feature.User.Types     (RegisterBody (..), RegisterError (..))
 import qualified Feature.User.UserModel as User
-import           Protolude
+import           Infrastructure.Maybe   (maybeToEither)
+import           Protolude              hiding (maybeToEither)
 
 insertUser :: User.User -> Action IO (Either RegisterError Value)
 insertUser user = do
-    userExists <- doesUserExist $ User.unwrapUsername $ User.username user
+    userExists <- DB.doesUserExist $ User.unwrapUsername $ User.username user
 
-    if userExists then do
-        doc <- DB.insertUser user
-        pure $ Right doc
+    if not userExists then do
+        Right <$> DB.insertUser user
     else
         pure $ Left UserAlreadyExists
+
+mkUser :: Time.UTCTime -> RegisterBody -> Either RegisterError User.User
+mkUser dateNow req =
+    maybeToEither ValidationFailed $ User.User
+        <$> (User.mkUsername $ username req)
+        <*> (User.mkPassword $ password req)
+        <*> pure dateNow
