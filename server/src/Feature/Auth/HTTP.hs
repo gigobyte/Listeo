2adbcec6d7a3a127957b3875fc1bb05876998995
gigobyte <gigobest2@gmail.com>
2019-01-20 14:04:11 +0200

    Auth middleware return user instead of just ID

--- server/src/Feature/Auth/HTTP.hs
+++ server/src/Feature/Auth/HTTP.hs
@@ -9,21 +9,24 @@ import Web.Scotty.Trans (ActionT)
 import Web.Scotty.Trans as ScottyT
 import Control.Monad.Trans.Maybe
 import Network.HTTP.Types.Status
+import Feature.User.Service (UserRepo, findUser)
+import Feature.User.DB (User)
 import Infrastructure.Utils (liftMaybe)
 import qualified Web.JWT as JWT
 
-requireUser :: (Monad m) => ActionT LText m Text
+requireUser :: (UserRepo m) => ActionT LText m User
 requireUser = do
   maybeAuthHeader <- ScottyT.header "Authorization"
 
-  maybeUserId     <- runMaybeT $ do
+  maybeUser       <- lift $ runMaybeT $ do
     authHeader    <- liftMaybe maybeAuthHeader
     unverifiedJwt <- liftMaybe $ JWT.decode $ toStrict $ drop 7 $ authHeader
     subject       <- liftMaybe $ JWT.sub $ JWT.claims unverifiedJwt
+    user          <- MaybeT $ findUser $ JWT.stringOrURIToText subject
 
-    return $ JWT.stringOrURIToText subject
+    return user
 
-  case maybeUserId of
+  case maybeUser of
     Just u  -> pure u
     Nothing -> do
       ScottyT.status status401
