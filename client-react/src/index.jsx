880a64ed44b8b6e83e5464a8ba9b764d4179a1db
gigobyte <gigobest2@gmail.com>
2019-11-04 21:14:43 +0200

    Remove compiled files

--- client-react/src/index.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = __importDefault(require("react"));
-var react_dom_1 = __importDefault(require("react-dom"));
-var App_1 = require("./App");
-react_dom_1.default.render(<App_1.App />, document.querySelector('main'));
