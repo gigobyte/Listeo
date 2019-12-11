d3c5a997a7527a5d903bdc6054dff4ac83511692
Stanislav Iliev <gigobest2@gmail.com>
2019-12-11 19:00:25 +0200

    Move CreatePlaylistBody inside file, refactor mkInsertPlaylistTags #5

--- server/src/Feature/Playlist/CreatePlaylist/CreatePlaylistService.hs
+++ server/src/Feature/Playlist/CreatePlaylist/CreatePlaylistService.hs
@@ -1,17 +1,35 @@
 module Feature.Playlist.CreatePlaylist.CreatePlaylistService where
 
 import Protolude
-import Data.Aeson (decode)
+import Data.Aeson
 import Control.Monad.Except (liftEither)
 import Infrastructure.Utils.Id (Id)
-import Feature.Playlist.CreatePlaylist.CreatePlaylistBody (CreatePlaylistBody)
-import Feature.Playlist.CreatePlaylist.CreatePlaylistResult (CreatePlaylistError(..))
-import qualified Feature.Playlist.CreatePlaylist.CreatePlaylistBody as CreatePlaylistBody
-import Feature.Playlist.Playlist (Playlist)
+import Feature.Playlist.Playlist (Playlist, PlaylistStyle, PlaylistPrivacy)
+import Feature.Playlist.CreatePlaylist.CreatePlaylistResult
+  (CreatePlaylistError(..))
 import Feature.Playlist.PlaylistRepoClass (PlaylistRepo(..), InsertPlaylist(..))
 import Feature.PlaylistTag.PlaylistTagRepoClass
   (PlaylistTagRepo(..), InsertPlaylistTag(..))
 import qualified Data.Text as T
+
+data CreatePlaylist = CreatePlaylist
+    { createPlaylistName :: Text
+    , createPlaylistTags :: [Text]
+    , createPlaylistPrivacy :: PlaylistPrivacy
+    , createPlaylistStyle :: PlaylistStyle
+    }
+
+instance FromJSON CreatePlaylist where
+  parseJSON = withObject "createPlaylist" $ \o ->
+    CreatePlaylist
+      <$> o
+      .:  "name"
+      <*> o
+      .:  "tags"
+      <*> o
+      .:  "privacy"
+      <*> o
+      .:  "style"
 
 createPlaylist
   :: (PlaylistRepo m, PlaylistTagRepo m)
@@ -21,27 +39,27 @@ createPlaylist rawBody = runExceptT $ do
   body               <- liftEither $ parseBody rawBody
   playlist           <- liftEither $ mkInsertPlaylist body
   insertedPlaylistId <- lift $ insertPlaylist playlist
-  tags               <-
-    liftEither
-    $   sequence
-    $   mkInsertPlaylistTag
-    <$> (CreatePlaylistBody.tags body)
+  tags               <- liftEither $ mkInsertPlaylistTags body
 
   lift $ forM_ tags (insertPlaylistTag insertedPlaylistId)
 
   return insertedPlaylistId
 
-parseBody :: LByteString -> Either CreatePlaylistError CreatePlaylistBody
+parseBody :: LByteString -> Either CreatePlaylistError CreatePlaylist
 parseBody body = maybeToRight InvalidRequest (decode body)
 
-mkInsertPlaylist
-  :: CreatePlaylistBody -> Either CreatePlaylistError InsertPlaylist
+mkInsertPlaylist :: CreatePlaylist -> Either CreatePlaylistError InsertPlaylist
 mkInsertPlaylist req =
   maybeToRight ValidationFailed
     $   InsertPlaylist
-    <$> (validatePlaylistName $ CreatePlaylistBody.name req)
-    <*> pure (CreatePlaylistBody.style req)
-    <*> pure (CreatePlaylistBody.privacy req)
+    <$> (validatePlaylistName $ createPlaylistName req)
+    <*> pure (createPlaylistStyle req)
+    <*> pure (createPlaylistPrivacy req)
+
+mkInsertPlaylistTags
+  :: CreatePlaylist -> Either CreatePlaylistError [InsertPlaylistTag]
+mkInsertPlaylistTags body =
+  sequence $ mkInsertPlaylistTag <$> (createPlaylistTags body)
 
 mkInsertPlaylistTag :: Text -> Either CreatePlaylistError InsertPlaylistTag
 mkInsertPlaylistTag tagName =
